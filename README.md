99588202be3311b28bf0306bc84a87802c40396d
Edward Kim <edk217@gmail.com>
2012-04-30 20:14:36 -0400

    first_commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Blogger
+Rails tutorial from [jumpstart labs](http://tutorials.jumpstartlab.com/projects/blogger.html)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '2ec95ef1e81572ff3bc5514159b53b0ad2995233ac1a03d08e0ebdd61f55fccfcab75bd59b7e9c52a55bf02383b0cb5afae1a912cd890a38dfdf7ca55ba72ad5'
